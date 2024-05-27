ce78d7ca19ecc7d377dc3ed6b79477d49b1208dc
Nathan Nye <nathan@nye.design>
2024-05-27 09:27:55 -0400

    spacing object format

--- rules/spacing.js
+++ rules/spacing.js
@@ -1,7 +1,10 @@
 const { toRem } = require('../utils')
 
 const arr = Array.from({ length: 1001 }, (_, i) => i)
+const spacing = {}
 
-const spacing = arr.map((i) => ({ [i]: toRem(i) }))
+arr.forEach((i) => {
+  spacing[i] = toRem(i)
+})
 
 module.exports = spacing
